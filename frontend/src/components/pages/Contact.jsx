b79cf3f2494b2af4ef1cc550df3f3b65eb615341
Khushi0204 <khushisharma20190@gmail.com>
2024-09-12 19:25:26 +0530

    2

--- frontend/src/components/pages/Contact.jsx
+++ frontend/src/components/pages/Contact.jsx
@@ -93,7 +93,7 @@ const ContactUs = () => {
 
       <div className="contact-right">
         {/* Your existing contact form */}
-        <form action="https://formspree.io/f/meojworo" method="post" netlify>
+        <form action="https://formspree.io/f/meojworo" method="post" data-netlify="true">
           <div>
             <label>Name:</label>
             <input type="text" id="name" name="name" placeholder="Enter Your Name" />
