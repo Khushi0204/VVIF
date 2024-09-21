3b9ccb53701e6022a3d4babd92e4694cddcd1745
Anubhav <xcaliber704@gmail.com>
2024-09-21 16:55:48 +0530

    login and signup feature complete

--- backend/server/models/User.js
+++ backend/server/models/User.js
@@ -1,15 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const UserSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-//   Phone: { type: String, required: true, unique: true},
-// });
-
-// module.exports = mongoose.model('User', UserSchema);
-
-
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
